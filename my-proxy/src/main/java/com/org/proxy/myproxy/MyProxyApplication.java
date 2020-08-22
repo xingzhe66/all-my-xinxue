e0dc8934b8a88a512f432fe64461ddf737553e5b
sunllm <sunlonglong159@163.com>
2020-08-22 12:00:04 +0800

    #技术整理

---
+++ my-proxy/src/main/java/com/org/proxy/myproxy/MyProxyApplication.java
@@ -0,0 +1,13 @@
+package com.org.proxy.myproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProxyApplication.class, args);
+    }
+
+}
