c6513568371491c66fcaf49314c3e5c0b350edcc
sunllm <sunlonglong159@163.com>
2020-09-27 23:37:38 +0800

    #生产者模块提交

---
+++ my-readxml/src/main/java/com/my/readxml/demo/MyReadxmlApplication.java
@@ -0,0 +1,13 @@
+package com.my.readxml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReadxmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyReadxmlApplication.class, args);
+    }
+
+}
---
+++ my-readxml/src/main/java/com/my/readxml/demo/xml/MainXml.java
@@ -0,0 +1,40 @@
+package com.my.readxml.demo.xml;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.Iterator;
+
+/**
+ * @author sunlonglong
+ * 23:16
+ * 境由心生,事在人为
+ */
+public class MainXml {
+    public static void main(String[] args) throws Exception {
+
+        File f= new File("D:\\programe\\alltechnology\\my-readxml\\src\\main\\resources\\myxml.xml");
+        if(!f.exists()){
+            System.out.println("配置文件不存在");
+            return;
+        }
+
+        SAXReader reader = new SAXReader();
+
+        Document doc = reader.read(f);
+
+        Element rootElement = doc.getRootElement();
+
+        Element foo;
+
+        for(Iterator i = rootElement.elementIterator("BOOK"); i.hasNext();){
+
+            foo = (Element)i.next();
+            System.out.println("NAME的值："+foo.elementText("NAME"));
+            System.out.println("PRICE的值："+foo.elementText("PRICE"));
+            System.out.println("NUM的值："+foo.elementText("NUM"));
+        }
+    }
+}
