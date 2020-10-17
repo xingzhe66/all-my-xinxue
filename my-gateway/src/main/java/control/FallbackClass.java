bf88d2b215dbbff223391ecc5071e891be2e04c4
sunllm <sunlonglong159@163.com>
2020-10-17 10:45:08 +0800

    #增加网关配置

---
+++ my-gateway/src/main/java/control/FallbackClass.java
@@ -0,0 +1,20 @@
+package control;
+
+
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * @author sunlonglong
+ * 23:26
+ * 境由心生,事在人为
+ */
+@RestController
+public class FallbackClass {
+
+
+}
